5f38b2b52c1b2876a4e308abeea68e0bedecd619
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2023-04-22 16:28:42 -0500

    Make graph_common_data_fields() function

--- dcns/parse_data.py
+++ dcns/parse_data.py
@@ -24,9 +24,6 @@ trips = pd.read_csv(DATA_DIR / "trips.txt")
 stop_times = pd.read_csv(DATA_DIR / "stop_times.txt")
 stops = pd.read_csv(DATA_DIR / "stops.txt")
 
-# Read all files in DATA_DIR
-d = {f.stem: pd.read_csv(f) for f in DATA_DIR.iterdir()}
-
 
 # Convert stop times to actual times
 def time_string_to_timedelta(time_string):
@@ -41,24 +38,35 @@ stop_times["departure_time_sec"] = stop_times.departure_time.apply(
     time_string_to_timedelta
 ).dt.total_seconds()
 
+
 # %% Get the columns from each file for comparison
-cols = pd.DataFrame(data={name: pd.Series(d[name].columns.str.lower()) for name in d})
+def graph_common_data_fields(data_dir: Path, draw=True):
+    # Read all files in DATA_DIR
+    d = {f.stem: pd.read_csv(f) for f in data_dir.iterdir()}
 
-# %% Display a graph based on fields that link each file
-G = nx.Graph()
-G.add_nodes_from(cols.keys())
-for u, v in itertools.permutations(cols.keys(), 2):
-    common_fields = set(cols[u].dropna()) & set(cols[v].dropna())
-    if common_fields:
-        # print(common_fields)
-        G.add_edge(u, v, weight=len(common_fields), fields=",".join(common_fields))
+    cols = pd.DataFrame(
+        data={name: pd.Series(d[name].columns.str.casefold()) for name in d}
+    )
 
-G.remove_nodes_from(list(nx.isolates(G)))
+    # % Display a graph based on fields that link each file
+    G = nx.Graph()
+    G.add_nodes_from(cols.keys())
+    for u, v in itertools.permutations(cols.keys(), 2):
+        common_fields = set(cols[u].dropna()) & set(cols[v].dropna())
+        if common_fields:
+            # print(common_fields)
+            G.add_edge(u, v, weight=len(common_fields), fields=",".join(common_fields))
 
-# Draw
-pos = nx.spring_layout(G, 1)
-nx.draw(G, pos, with_labels=True)
-nx.draw_networkx_edge_labels(G, pos, edge_labels=nx.get_edge_attributes(G, "fields"))
+    G.remove_nodes_from(list(nx.isolates(G)))
+
+    if draw:
+        pos = nx.spring_layout(G, 1)
+        nx.draw(G, pos, with_labels=True)
+        nx.draw_networkx_edge_labels(
+            G, pos, edge_labels=nx.get_edge_attributes(G, "fields")
+        )
+
+    return G
 
 
 # %%
@@ -80,7 +88,7 @@ G = nx.DiGraph()
 
 r1 = routes.route_id[0]
 r1_trips = trips[trips.route_id == r1]
-# for trip_id in r1_trips["trip_id"].drop_duplicates():
+# for trip_id in list(r1_trips["trip_id"].drop_duplicates())[140:150]:
 for trip_id in tqdm(trips["trip_id"].drop_duplicates()):
     trip_stop_times = (
         stop_times[stop_times.trip_id == trip_id]
@@ -97,9 +105,6 @@ for trip_id in tqdm(trips["trip_id"].drop_duplicates()):
 
 
 # %%
-def latlon_to_pos(row):
-    return np.array(row.values)
-
 
 # Get stop locations based on latitude / longitude as a dictionary networkx format:
 # { node_id: np.array([x, y]), ... }
@@ -107,12 +112,13 @@ def latlon_to_pos(row):
 # { stop_id: np.array([stop_lon, stop_lat]), ... }
 stop_pos = (
     stops.set_index("stop_id")[["stop_lon", "stop_lat"]]
-    .apply(latlon_to_pos, axis=1)
+    .apply(lambda row: np.array(row.values), axis=1)
     .to_dict()
 )
 
 # Set node attributes
 nx.set_node_attributes(G, stop_pos, "pos")
+nx.set_node_attributes(G, stops.set_index("stop_id").stop_name.to_dict(), "name")
 
 # %% Average trip times for each node
 num_trips = {
@@ -133,6 +139,8 @@ nx.draw(G, stop_pos, node_size=5, width=0.5)
 
 
 # %% Write the graph to .gml
+# Edges may have a few hundred trips each which would clutter the .gml file so we purge
+# them first
 Gout = remove_edge_attrs(G.__class__(G), "trip_times")
 # Convert pos from numpy array to list so it can be exported as gml
 node_attr_ndarray_to_list(Gout, "pos")
