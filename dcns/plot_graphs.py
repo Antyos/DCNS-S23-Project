988f7e3cf95d9e95e1511d947ad983560ddbaf47
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2023-04-29 15:28:16 -0500

    Add costrings to plot_graphs()

--- dcns/plot_graphs.py
+++ dcns/plot_graphs.py
@@ -1,25 +1,39 @@
 from functools import partial
-from typing import Callable
+from typing import Callable, Optional
 
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 from matplotlib.animation import FuncAnimation
-from numpy.typing import NDArray
 
-from dcns.graph_utils import Graph, Node
+from dcns.graph_utils import Graph, Node, PosDict
 from dcns.pathfinding import SearchGenerator, pathfind_steps
 
 
 def plot_graph(
     graph: Graph,
-    pos: dict,
+    pos: PosDict,
     ax: plt.Axes,
     min_edge_length=0.007,
-    style=None,
+    style: Optional[dict] = None,
     edges=None,
 ):
-    """Plot a graph but without edges below a threshold."""
+    """Plot a graph but without edges below a threshold.
+
+    Parameters
+    ----------
+    graph: nx.Graph | nx.DiGraph
+    pos: dic of positions
+    ax: plt.Axes
+    min_edge_length: float
+        Edges below this length will not be plotted. Increase to plot faster, but display
+        fewer edges
+    style: dict (optional)
+        Style for graph
+    edges: iterable of edges (optional)
+        If trying to plot a subgraph, there is a bug where the edges will not display.
+        If that is the case, pass in the edges from the original graph here.
+    """
     if style is None:
         style = {}
     # Only draw edges over a threshold for faster displaying. It is very dumb that we
@@ -54,8 +68,32 @@ def plot_pathfinding(
     min_edge_length=0.007,
     sample_steps: tuple[int, int] = (200, 10),
     title: str = "",
-    style=None,
+    style: Optional[dict] = None,
 ):
+    """Animates the pathfinding steps
+
+    Parameters
+    ----------
+    graph: nx.Graph | nx.DiGraph
+    search_func: func(Graph, Node, Node) -> SearchGenerator
+        Used to generate the pathfinding steps
+    start: Node
+        Starting node
+    end: Node
+        Ending node
+    min_edge_length: float (default, 0.007)
+        Edges below this length will not be plotted. Increase to plot faster, but display
+        fewer edges
+    sample_steps: tuple of (int, int)
+        How many iterations of the search or back-tracing phases do we do before adding
+        a frame to the animation.
+    style: dict
+        Style of graph
+
+    Returns:
+    --------
+    matplotlib.animation.FuncAnimation
+    """
     pos = nx.get_node_attributes(graph, "pos")
 
     if style is None:
@@ -84,7 +122,8 @@ def plot_pathfinding(
             ax=ax,
             nodelist=searched_nodes,
             node_color="tab:orange",
-            **style,
+            # Pylance doesn't know that style==None has already been taken care of
+            **style,  # type: ignore
         )
         nx.draw_networkx_nodes(
             graph,
@@ -92,14 +131,15 @@ def plot_pathfinding(
             ax=ax,
             nodelist=path_nodes,
             node_color="tab:green",
-            **style,
+            # Pylance doesn't know that style==None has already been taken care of
+            **style,  # type: ignore
         )
 
     if title:
         # fig.suptitle(title)
         ax.set_title(title)
     fig.tight_layout()
-    fig.subplots_adjust(left=0, right=1, wspace=None)
+    fig.subplots_adjust(left=0, right=1, wspace=0)
 
     # Create an animation object
     return FuncAnimation(
